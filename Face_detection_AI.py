85e6b89bbd03e73290605954b5dbcc99f4e6687b
NumpandasPy <danikmaksimov6@gmail.com>
2024-10-20 20:48:56 +1030

    Add files via upload
    Duplicate data for master branch, master data in master branch

---
+++ Face_detection_AI.py
@@ -0,0 +1,43 @@
+from pickletools import uint8
+
+import cv2
+import numpy
+import numpy as np
+from numpy import dtype
+import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import pylab as pl
+import easyocr
+
+
+
+
+# VideoCamera detection faces
+cap = cv2.VideoCapture(0)
+cap.set(3, 500)
+cap.set(4, 300)
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 500)
+cap.set(4, 300)
+
+while True:
+   success, img = cap.read()
+   #  bin changes
+   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+   faces = cv2.CascadeClassifier('faces.xml')
+   # 1.98 - 2.08 (detection 1 - 5 faces )
+   results = faces.detectMultiScale(gray, scaleFactor=1.98, minNeighbors=2, minSize=(30, 30))
+
+   for (x, y, w, h) in results:
+       cv2.rectangle(img, (x, y), (x + w, y + h), (120, 31, 255), thickness=2)
+
+   cv2.imshow('Result', img)
+
+   if cv2.waitKey(1) & 0xFF == ord('q'):
+       break
+
+cv2.imshow('Result', img)
+
+cv2.waitKey(0)
